3c37f67dcff2e1a812f3428840d7736f31b42491
qhl <qhlhelo@163.com>
2018-08-27 21:01:20 +0800

    some update

--- src/main/scala/studentTeacher/TeacherPreTest.scala
+++ src/main/scala/studentTeacher/TeacherPreTest.scala
@@ -4,8 +4,11 @@ import akka.actor.ActorSystem
 import akka.testkit.{ImplicitSender, TestActors, TestKit}
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
 
-class TeacherPreTest extends TestKit(ActorSystem("TeacherPreTest")) with ImplicitSender
-	with WordSpecLike with Matchers with BeforeAndAfterAll {
+class TeacherPreTest extends TestKit(ActorSystem("TeacherPreTest"))
+//	with ImplicitSender
+	with WordSpecLike
+	with Matchers
+	with BeforeAndAfterAll {
 	
 	override def afterAll: Unit = {
 		TestKit.shutdownActorSystem(system)
